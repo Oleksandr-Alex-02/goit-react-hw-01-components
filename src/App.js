df7da00b52337e8604db24ca438a394829a0fec5
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-09-27 21:14:35 +0300

    Profile_+

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Profile from "./componenst/Profile.react/Profile";
+import user from "./user.json";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      ;
+    </div>
+  );
+}
