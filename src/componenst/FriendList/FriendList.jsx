5140308080076d0009f0d4a6b9a572928d2066b4
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-10-01 19:11:36 +0300

    -

--- src/componenst/FriendList/FriendList.jsx
+++ src/componenst/FriendList/FriendList.jsx
@@ -1,11 +1,19 @@
 import PropTypes from 'prop-types';
 
+const alertStyles = {
+    backgroundColor: "rgb(21, 129, 39)",
+};
+
+const colors = {
+    backgroundColor: "rgb(83, 49, 206)"
+}
+
 export default function Friend(props) {
     const { friends } = props
 
     const friendsList = friends.map((friend) =>
         < li className="friends__item" key={friend.id}>
-            <span className="friends__status">{friend.isOnline}</span>
+            <span className="friends__status" style={friend.isOnline > true ? alertStyles : colors}></span>
             <img className="friends__avatar" src={friend.avatar} alt={friend.name} width="48" />
             <p className="friends__name">{friend.name}</p>
         </li >
