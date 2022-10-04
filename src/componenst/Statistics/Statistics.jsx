2af4dc1a4632ac152e8fd370ce3bdbd7b66f0678
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-10-04 13:10:57 +0300

    finich

--- src/componenst/Statistics/Statistics.jsx
+++ src/componenst/Statistics/Statistics.jsx
@@ -2,25 +2,23 @@ import PropTypes from 'prop-types';
 
 function bacColor() {
   const colorRandom = Math.floor(Math.random() * 16777215).toString(16)
-  return colorRandom
-}
-
-const background = {
-  backgroundColor: bacColor(),
+  return `#${colorRandom}`
 }
 
 export default function Statistics(props) {
   const { stats, title } = props
 
-  const li = stats.map((arr) =>
-    <li className="item" style={background} key={arr.id}>
+  const li = stats.map((arr) => {
+    const background = bacColor();
+
+    return < li className="item" style={{ backgroundColor: background }} key={arr.id} >
       <span className="label">{arr.label}</span>
       <span className="percentage">{arr.percentage}%</span>
-    </li>
-  );
+    </li >
+  });
 
   if (li.length !== 0) {
-    return (<section class="statistics">
+    return (<section className="statistics">
       <div className="stats_cont">
         <h2 className="title">{title}</h2>
         <ul className="stat-list">
