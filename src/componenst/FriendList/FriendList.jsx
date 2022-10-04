596d28ee9d131187e3691f14ba53e210e47c7699
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-10-04 14:39:06 +0300

    finich-2

--- src/componenst/FriendList/FriendList.jsx
+++ src/componenst/FriendList/FriendList.jsx
@@ -3,17 +3,16 @@ import PropTypes from 'prop-types';
 export default function Friend(props) {
     const { friends } = props
 
-    const friendsList = friends.map((friend) =>
-        < li className="friends__item" key={friend.id}>
-            {friend.isOnline === true ? (
-                <span className="friends__status friend-online"></span>
-            ) : (
-                <span className="friends__status friend-ofOnline"></span>
-            )}
+    const friendsList = friends.map((friend) => {
+        const style = friend.isOnline ? 'friend-online' : 'friend-ofOnline';
+        const frientStyle = `friends__status + ${style}`;
+
+        return < li className="friends__item" key={friend.id} >
+            <span className={frientStyle}></span>
             <img className="friends__avatar" src={friend.avatar} alt={friend.name} width="48" />
             <p className="friends__name">{friend.name}</p>
         </li >
-    )
+    })
 
     return (
         <div className='friends'>
