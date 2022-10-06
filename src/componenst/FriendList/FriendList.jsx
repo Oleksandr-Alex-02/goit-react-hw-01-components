4d8a07be795cb10e351137498293a977831717b0
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-10-06 15:15:29 +0300

    finich

--- src/componenst/FriendList/FriendList.jsx
+++ src/componenst/FriendList/FriendList.jsx
@@ -7,13 +7,9 @@ export default function Friend(props) {
 
     const friendsList = friends.map((friend) => {
 
+        return < li className={css.friends__item} key={friend.id} >
 
-        return < li className={
-            clsx(
-                // "css.friends__status",
-                friend.isOnline ? "friend__online" : "css.friend__ofOnline",
-            )} key={friend.id} >
-            <span className={css.friends__status}></span>
+            <span className={clsx(css.friends__status, friend.isOnline ? css.friend__online : css.friend__ofOnline)}></span>
             <img className={css.friends__avatar} src={friend.avatar} alt={friend.name} width="48" />
             <p className={css.friends__name}>{friend.name}</p>
         </li >
